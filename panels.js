35608d909df48fe09bb0a0bdce8797cafb28cfbb
JayceH <jayceh@mail.usf.edu>
2019-05-20 15:12:14 -0400

    Added panels.js which starts to add functionality for panels

---
+++ panels.js
@@ -0,0 +1,8 @@
+//Function to get the mouse position anywhere.
+//Can be used in any HTML tag
+function getMousePosition(event) {
+    return {
+        x: event.clientX,
+        y: event.clientY
+    }
+}
