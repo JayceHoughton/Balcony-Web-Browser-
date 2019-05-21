0d07c6e2a43d22e05f7dd1e0670478445750578e
Jayce Houghton <jayceh@mail.usf.edu>
2019-05-20 20:54:56 -0400

    Proof of concept. Can now draw windows on the canvas. Click on the window, then drag mouse, you will see a box being made. Release to finish the drawing. For now, hard coded Youtube window will show up inside of the rectangle.

--- canvas.js
+++ canvas.js
@@ -5,6 +5,11 @@ ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+window.onresize = function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
 //Function to get the position of the mouse on the Canvas
 //Takes a Canvas and an event
 function getCanvasMousePosition(canvas, event) {
@@ -22,8 +27,50 @@ function drawMouseRect(canvas, event) {
     ctx.stroke()
 }
 
-document.getElementById("theCanvas").onclick = function() {
+function drawDragRect(canvas, event, leftCorner) {
+    rightCorner = getCanvasMousePosition(canvas, event)
+    ctx.beginPath()
+    ctx.rect(leftCorner.x, leftCorner.y, rightCorner.x - leftCorner.x, rightCorner.y - leftCorner.y)
+    console.log(rightCorner.x - leftCorner.x)
+    ctx.stroke()
+    ctx.closePath()
+    return {
+        x: leftCorner.x,
+        y: leftCorner.y,
+        width: rightCorner.x - leftCorner.x,
+        height: rightCorner.y - leftCorner.y
+    }
+}
+
+/*document.getElementById("theCanvas").onclick = function() {
     coordinates = getCanvasMousePosition(canvas, event)
     document.getElementById("panel").innerHTML = "X is: " + coordinates.x + " and Y is: " + coordinates.y
     drawMouseRect(canvas, event)
+}*/
+
+document.getElementById("theCanvas").onmousedown = function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    //Sets the leftcorner of the rectangle to the current mouse position
+    leftCorner = getCanvasMousePosition(canvas, event)
+
+    //Function to be called by the event listener. Repeatedly calls drawDragRect to redraw rectangle over and over again
+    function drawRect() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        drawDragRect(canvas, event, leftCorner)
+    }
+
+    //Adds lister to mouse movement. Makes it so that drawRect is called repeatedly for dynamic rectangle drawing
+    document.addEventListener('mousemove', drawRect)
+
+    //When the mouse is released the final rectangle is drawn
+    document.getElementById("theCanvas").onmouseup = function() {
+
+        //Removes the event listener form mouse movement to stop the rectangle from being repeatedly drawn
+        document.removeEventListener('mousemove', drawRect)
+
+        //Gets the dimensions of the final panel
+        panelDims = drawDragRect(canvas, event, leftCorner)
+        createNewPanel(panelDims.x, panelDims.y, panelDims.width, panelDims.height, 'https://www.youtube.com/')
+    }
 }
